a704a1f1aff99344fae010b37bce67a9a36d30f6
Antonio Neto <antoniogfneto92@hotmail.com>
2024-04-25 19:59:20 -0300

    testando netlify

--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page.component';
 
 
-
 @NgModule({
   declarations: [
     MainPageComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    MainPageComponent
   ],
 })
 export class MainPageModule { }
